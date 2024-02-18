a0ff98007e03820860d0c09365dacf23dc44e133
Hill <125409221+NARI1108@users.noreply.github.com>
2024-02-18 14:07:54 +0330

    Using this method, we find the components of the visual design and associate them with the corresponding variables. This allows access and control over visual design components in Java code.

--- app/src/main/java/com/example/online_pharmcy/MainActivity.java
+++ app/src/main/java/com/example/online_pharmcy/MainActivity.java
@@ -17,7 +17,29 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        findViews();
+    }
+//    This method actually connects views and layers from an XML layer to this layer.
+    public void findViews(){
+//       The ID related to the RelativeLayout.
+        rlt_drugs = findViewById(R.id.rlt_drugs);
+        rlt_sickness = findViewById(R.id.rlt_sickness);
+        rlt_honey = findViewById(R.id.rlt_honey);
+        rlt_avarez = findViewById(R.id.rlt_avarez);
+        rlt_bmi = findViewById(R.id.rlt_bmi);
+        rlt_tashkhis = findViewById(R.id.rlt_tashkhis);
+//       The ID related to the TextView.
+        txt_drugs_tedad = findViewById(R.id.txt_drugs_tedad);
+        txt_sickness_tedad = findViewById(R.id.txt_sickness_tedad);
+        txt_honey_tedad = findViewById(R.id.txt_honey_tedad);
+        txt_avarez_tedad = findViewById(R.id.txt_avarez_tedad);
+        txt_tashkhis_tedad = findViewById(R.id.txt_tashkhis_tedad);
+//       The ID related to the DrawerLayout.
+        drawer_layout = findViewById(R.id.drawer_layout);
+//       The ID related to the Toolbar.
+        toolbar = findViewById(R.id.toolbar);
+//        The ID related to the NavigationView.
+        navi_view = findViewById(R.id.navi_view);
     }
 //  In short, this method is used to perform operations related to
 //  navigation menu items, such as starting an Activity, displaying
