851eeec1b813143516f10692c6bd13813631f368
Hill <125409221+NARI1108@users.noreply.github.com>
2024-02-18 14:01:16 +0330

    This method implements an event called onNavigationItemSelected for a navigation menu. This event is called when the user selects an item from the navigation menu.

--- app/src/main/java/com/example/online_pharmcy/MainActivity.java
+++ app/src/main/java/com/example/online_pharmcy/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.online_pharmcy;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+//  In short, this method is used to perform operations related to
+//  navigation menu items, such as starting an Activity, displaying
+//  messages, and closing the navigation menu.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int Ids = item.getItemId();
+        switch (Ids) {
+            case R.id.favorite:
+                startActivity(new Intent(MainActivity.this,FavoriteActivity.class));
+                break;
+            case R.id.settings:
+                startActivity(new Intent(MainActivity.this,SettingActivity.class));
+                break;
+            case R.id.About:
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("درباره مـــا")
+                        .setMessage(R.string.About_us)
+                        .show();
+                break;
+            case R.id.resource:
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("منــابــع")
+                        .setMessage(R.string.Resource)
+                        .show();
+                break;
+            case R.id.exit:
+                finish();
+                break;
+        }
+        drawer_layout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
