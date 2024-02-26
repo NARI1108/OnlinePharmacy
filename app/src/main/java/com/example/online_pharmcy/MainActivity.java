d8c173b571a63c442037ddcd499f4e3123218723
Hill <125409221+NARI1108@users.noreply.github.com>
2024-02-26 20:41:21 +0330

    This method is generally used in Android applications, especially when performing concurrent or long-running operations that require displaying the progress of that operation to the user.

--- app/src/main/java/com/example/online_pharmcy/MainActivity.java
+++ app/src/main/java/com/example/online_pharmcy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -482,5 +484,12 @@ private class GetAvarezData extends AsyncTask<Void,Void,String> {
         }else
             txt_sickness_tedad.setVisibility(View.GONE);
 
+    }
+//    the progressDialog display method
+    private void showMessage(Context context, String msg){
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage(msg);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
     }
 }
