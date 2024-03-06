a6532579aa5ad74ec9f02a606e0fb877e7aec96a
Hill <125409221+NARI1108@users.noreply.github.com>
2024-03-06 14:28:53 +0330

    In general, this method is used to set click events on list items displayed using the RecyclerView and performs different operations based on the data type and search state.

--- app/src/main/java/com/example/online_pharmcy/FehrestActivity.java
+++ app/src/main/java/com/example/online_pharmcy/FehrestActivity.java
@@ -1,5 +1,6 @@
 package com.example.online_pharmcy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,8 +22,11 @@ public class FehrestActivity extends AppCompatActivity {
     int dataType;
     String activity;
     ArrayList<String> names_list;
-    Adapter  adapter;
-    boolean  isSearching = false, foundSickness = false;
+    Adapter adapter;
+    boolean isSearching = false, foundSickness = false;
+    ArrayList<String> search_list = new ArrayList<>();
+    ArrayList<String> sickness_names_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,52 +35,66 @@ public class FehrestActivity extends AppCompatActivity {
         request_Catch();
         adapter();
         setEdt_search();
+        setRcl_show();
     }
-//    this method is used to find and associate views in the application page.
-    public void findViews(){
+
+    //    this method is used to find and associate views in the application page.
+    public void findViews() {
         rcl_show = findViewById(R.id.rcl_show);
         txt_title = findViewById(R.id.txt_title);
-        edt_search = findViewById(R.id. edt_search);
+        edt_search = findViewById(R.id.edt_search);
     }
-//In short, this method is used to get the required information from the database based on the values received from the previous activity.
-    private void request_Catch(){
+    //In short, this method is used to get the required information from the database based on the values received from the previous activity.
+    private void request_Catch() {
 //      With the first variable, we specify which topic information should be taken, and with the second variable, we specify whether it came to this page from the main activity or from the interests.
-        dataType = getIntent().getIntExtra("datatype",0);
+        dataType = getIntent().getIntExtra("datatype", 0);
         activity = getIntent().getStringExtra("activity");
 
         dataBaseManager = new DataBaseManager(this);
 //       We made a condition that according to the desired subject, the request will be made to the database and the required information will be obtained
-        if (dataType== MainActivity.DRUGS){
-            if (activity.equals("main")){names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_DURGS);}else {
-                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_DURGS);}
+        if (dataType == MainActivity.DRUGS) {
+            if (activity.equals("main")) {
+                names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_DURGS);
+            } else {
+                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_DURGS);
+            }
             txt_title.setVisibility(View.VISIBLE);
             txt_title.setText(R.string.searchText_drugs);
-        }else if (dataType== MainActivity.SICKNESS){
-            if (activity.equals("main")){names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_SICKNESS);}else {
-                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_SICKNESS);}
+        } else if (dataType == MainActivity.SICKNESS) {
+            if (activity.equals("main")) {
+                names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_SICKNESS);
+            } else {
+                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_SICKNESS);
+            }
             txt_title.setVisibility(View.VISIBLE);
             txt_title.setText(R.string.searchText_Sickness);
-        }else if (dataType== MainActivity.HONEY){
-            if (activity.equals("main")){names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_HONEY);}else {
-                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_HONEY);}
-        }else if (dataType== MainActivity.AVAREZ){
-            if (activity.equals("main")){names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_AVAREZ);}else {
-                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_AVAREZ);}
-        }else if (dataType== MainActivity.TASHKHIS){
+        } else if (dataType == MainActivity.HONEY) {
+            if (activity.equals("main")) {
+                names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_HONEY);
+            } else {
+                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_HONEY);
+            }
+        } else if (dataType == MainActivity.AVAREZ) {
+            if (activity.equals("main")) {
+                names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_AVAREZ);
+            } else {
+                names_list = dataBaseManager.showFavorite(DataBaseManager.TABLE_NAME_AVAREZ);
+            }
+        } else if (dataType == MainActivity.TASHKHIS) {
             names_list = dataBaseManager.showNames(DataBaseManager.TABLE_NAME_ALAYEM);
             txt_title.setVisibility(View.VISIBLE);
             txt_title.setText(R.string.searchText_Tashkhis);
             rcl_show.setVisibility(View.GONE);
         }
     }
-//    Setting the adapter and connecting it to the recycler view.
-    private void adapter(){
-        adapter = new Adapter(this,names_list);
-        rcl_show.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+    //    Setting the adapter and connecting it to the recycler view.
+    private void adapter() {
+        adapter = new Adapter(this, names_list);
+        rcl_show.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         rcl_show.setAdapter(adapter);
     }
-//    This method is used to set events related to text changes in a text field (EditText). This method adds information to the text field that defines a listener for field text changes.
-    private void setEdt_search(){
+    //    This method is used to set events related to text changes in a text field (EditText). This method adds information to the text field that defines a listener for field text changes.
+    private void setEdt_search() {
         edt_search.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -86,13 +104,48 @@ public class FehrestActivity extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable editable) {
-                foundSickness=false;
+                foundSickness = false;
                 searchName(editable.toString());
-                if (editable.toString().trim().length() == 0){
-                    isSearching=false;
-                }else {isSearching=true;}
-                if (dataType == MainActivity.TASHKHIS)txt_title.setVisibility(View.GONE);
+                if (editable.toString().trim().length() == 0) {
+                    isSearching = false;
+                } else {
+                    isSearching = true;
+                }
+                if (dataType == MainActivity.TASHKHIS) txt_title.setVisibility(View.GONE);
             }
         });
+    }
+//    This method is used to set events for clicking on items in a list displayed using a RecyclerView.
+    private void setRcl_show() {
+        rcl_show.addOnItemTouchListener(new RecyclerItemClick(this, new RecyclerItemClick.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                if (dataType != MainActivity.TASHKHIS) {
+                    String name;
+                    if (isSearching) name = search_list.get(position);
+                    else name = names_list.get(position);
+                    Intent intent = new Intent(FehrestActivity.this, ShowDataActivity.class);
+                    intent.putExtra("datatype", dataType);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
+                if (dataType == MainActivity.TASHKHIS && foundSickness) {
+                    Intent intent = new Intent(FehrestActivity.this, ShowDataActivity.class);
+                    intent.putExtra("datatype", dataType);
+                    intent.putExtra("name", sickness_names_list.get(position));
+                    startActivity(intent);
+                } else if (dataType == MainActivity.TASHKHIS && !foundSickness) {
+                    if (sickness_names_list != null) sickness_names_list.clear();
+                    sickness_names_list = dataBaseManager.searchSickness(search_list.get(position));
+                    adapter = new Adapter(FehrestActivity.this, sickness_names_list);
+                    rcl_show.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    foundSickness = true;
+                    txt_title.setVisibility(View.VISIBLE);
+                    txt_title.setText(getString(R.string.List_sickness) + search_list.get(position) + getString(R.string.display));
+                }
+            }
+        }));
     }
 }
