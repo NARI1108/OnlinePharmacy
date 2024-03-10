a0feab59efec1ec155995f8c212fcee8218ea360
Hill <125409221+NARI1108@users.noreply.github.com>
2024-03-10 15:15:04 +0330

    The "setting" method is used for settings and configuration in the program. The function of this method includes getting the previous settings from the shared memory, resetting the saved settings, and setting and connecting the spinner adapter.

--- app/src/main/java/com/example/online_pharmcy/SettingActivity.java
+++ app/src/main/java/com/example/online_pharmcy/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.example.online_pharmcy;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class SettingActivity extends BaseActivity {
 
@@ -9,6 +11,7 @@ public class SettingActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         findViews();
+        settingAgain();
     }
 //     this method is used to find and associate views in the application page.
     private void findViews(){
@@ -18,5 +21,21 @@ public class SettingActivity extends BaseActivity {
         seek_size = findViewById(R.id.seekbar_size);
         seek_space = findViewById(R.id.seekbar_space);
         mySwitch = findViewById(R.id.mySwitch);
+    }
+//  This method named "setting" is used for various settings and configurations in the program.
+    private void settingAgain(){
+//      Getting saved settings.
+        getSharedpref();
+//      Resetting saved settings.
+        seek_size.setProgress(font_size);
+        txt_show.setTextSize(font_size);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/"+font+".ttf");
+        txt_show.setTypeface(typeface);
+        mySwitch.setChecked(isScreenOn);
+        txt_show.setLineSpacing(line_spacing,1.0f);
+//      Setting the spinner adapter and connecting it to the spinner.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,fontsList);
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spn_fonts.setAdapter(adapter);
     }
 }
